8ba8af4c054be8b3651887829d303135d5db04d7
Truongnhat9604bg <132559185+Truongnhat9604bg@users.noreply.github.com>
2023-07-30 22:57:12 +0700

    Chương trình đầu tiên

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using  namespace std;
+
+int main(){
+    int n;
+    cout << "nhap 1 so bat ky:";
+    cin >> n;
+    cout << n+1;
+    cout << "hello world!   ";
+    system("pause");
+}
