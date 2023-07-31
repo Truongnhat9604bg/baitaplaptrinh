7a2086c6dee8aad3c912b965193504a1474ddef0
Truongnhat9604bg <luongnhattruong2004@gmail.com>
2023-07-31 22:26:42 +0700

    lam bai tap tinh tong hieu tich thuong

--- main.cpp
+++ main.cpp
@@ -3,10 +3,14 @@ using  namespace std;
 
 int main(){
     
-    int n;
-    cout << "nhap 1 so bat ky:" << endl;
-    cin >> n;
-    cout << n+1 << endl;
-    cout << "hello world!   " << endl;
+    int tong,hieu,tich,a,b;
+    float thuong;
+    cout << "nhap a,b"<< endl;
+    cin >> a >> b;
+    tong=a + b;
+    hieu=  a-b;
+    tich= a*b;
+    thuong= a/b;
+    cout << tong << " " << hieu << " " << tich << " " << thuong << endl;
     system("pause");
 }
