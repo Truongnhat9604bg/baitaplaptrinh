7a18004ab973fcb23b775290fc0ffcc95aaf111c
Truongnhat9604bg <luongnhattruong2004@gmail.com>
2023-08-01 16:50:03 +0700

    tính toán

--- main.cpp
+++ main.cpp
@@ -3,14 +3,15 @@ using  namespace std;
 
 int main(){
     
-    int tong,hieu,tich,a,b;
+    int tong,hieu,tich,du,a,b;
     float thuong;
     cout << "nhap a,b"<< endl;
     cin >> a >> b;
     tong=a + b;
     hieu=  a-b;
     tich= a*b;
-    thuong= a%b;
-    cout << tong << " " << hieu << " " << tich << " " << thuong << endl;
+    thuong= (float)a/b;
+    du= a%b;
+    cout << tong << " " << hieu << " " << tich << " " << thuong << " " << du << endl;
     system("pause");
 }
