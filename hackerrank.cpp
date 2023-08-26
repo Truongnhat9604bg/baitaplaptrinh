b1ff1e5f0a6387c3da36c375ca46cf325d4314f0
Lương Nhật Trường <132559185+truongnhat96@users.noreply.github.com>
2023-08-26 11:16:49 +0700

    Update hackerrank.cpp

--- hackerrank.cpp
+++ hackerrank.cpp
@@ -46,8 +46,6 @@ gets(n);
    
    system("pause");
 } */
-
-// bài toán bán vé xem phim
 #include <iostream>
 #include <string>
 using namespace std;
@@ -75,6 +73,8 @@ int main(){
  
 system("pause");
 } */
+
+// bài toán bán vé xem phim
 bool check(int x[],int t){
      int c25=0,c50=0;
      for(int i=0;i<t;i++){
